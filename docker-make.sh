0582fb26b8ad313672737da2b8b9f4d9166c90f5
root <root@util.volmax.dk>
2020-05-22 00:51:24 +0200

    hh

--- docker-make.sh
+++ docker-make.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-export NODE_RED_VERSION=$(grep -oE "\"node-red\": \"(\w*.\w*.\w*.\w*.\w*.)" package.json | cut -d\" -f4)
-
-echo "#########################################################################"
-echo "node-red version: ${NODE_RED_VERSION}"
-echo "#########################################################################"
-
 docker build --no-cache \
     --file dockerfile.yaml \
-    --tag testing:node-red-debian .
+    --tag opencreations:node-red-debian .
